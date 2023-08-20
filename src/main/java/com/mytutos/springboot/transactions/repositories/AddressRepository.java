902e60e6a322fc5d1703b2859a95ed176da3b132
marcus <ahmed.mehdi.2009@gmail.com>
2023-08-21 06:35:26 +0700

    initial commit

--- src/main/java/com/mytutos/springboot/transactions/repositories/AddressRepository.java
+++ src/main/java/com/mytutos/springboot/transactions/repositories/AddressRepository.java
@@ -1,7 +1,6 @@
 package com.mytutos.springboot.transactions.repositories;
 
 import com.mytutos.springboot.transactions.model.Address;
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
